a180d49a6b7f21561f5c36060fe1043c63470913
Niyaz Batyrshin <Niyaz.Batyrshin@gmail.com>
2019-06-21 17:34:55 +0300

    * handle form errors

--- projects/form-control-validation/src/lib/form-control-validation.directive.ts
+++ projects/form-control-validation/src/lib/form-control-validation.directive.ts
@@ -2,36 +2,45 @@ import {
   ComponentFactoryResolver,
   ComponentRef,
   Directive,
-  Injector, Input,
+  Host,
+  Injector,
+  Input,
   OnDestroy,
   OnInit,
+  Optional,
+  Renderer2,
   Self,
   Type,
   ViewContainerRef
 } from '@angular/core';
 import {merge, Subscription} from 'rxjs';
-import {FormControlName} from '@angular/forms';
+import {AbstractControlDirective, FormControlName, FormGroupDirective} from '@angular/forms';
 import {FormControlValidationComponent, IFormControlValidationComponent} from './form-control-validation.component';
 import {mapTo} from 'rxjs/operators';
 
 
 @Directive({
-  selector: '[nzFormControlValidation]'
+  selector: '[formControlName][nzFormControlValidation], [formGroup][nzFormControlValidation]'
 })
 export class FormControlValidationDirective implements OnInit, OnDestroy {
+  // tslint:disable-next-line:no-input-rename
   @Input('nzFormControlValidation') componentClass?: Type<IFormControlValidationComponent>;
   private defaultComponentClass: Type<IFormControlValidationComponent> = FormControlValidationComponent;
   private componentRef: ComponentRef<IFormControlValidationComponent>;
+  private control: AbstractControlDirective;
   private statusChangeSubscription: Subscription;
 
   constructor(private resolver: ComponentFactoryResolver, private container: ViewContainerRef,
-              @Self() private control: FormControlName) {
+              private render: Renderer2,
+              @Optional() @Self() private formControl: FormControlName,
+              @Host() private formGroup: FormGroupDirective) {
+    this.control = formControl ? formControl : formGroup;
   }
 
   ngOnInit(): void {
     this.statusChangeSubscription = merge(
       this.control.statusChanges,
-      this.control.formDirective.ngSubmit.pipe(mapTo('FORM_SUBMIT'))
+      this.formGroup.ngSubmit.pipe(mapTo('FORM_SUBMIT'))
     ).subscribe(status => {
       this.destroy();
       if (status !== 'VALID' && this.control.errors) {
@@ -56,7 +65,14 @@ export class FormControlValidationDirective implements OnInit, OnDestroy {
     const factory = this.resolver.resolveComponentFactory(this.componentClass || this.defaultComponentClass);
     const injector = Injector.create({providers: []});
     this.componentRef = this.container.createComponent(factory, 0, injector);
-    this.componentRef.instance.name = this.control.name;
+    if (!this.formControl) {  // insert form errors before controls
+      this.render.insertBefore(
+        this.container.element.nativeElement,
+        this.componentRef.location.nativeElement,
+        this.container.element.nativeElement.firstChild
+      );
+    }
+    this.componentRef.instance.name = this.formControl ? this.formControl.name : null;
     this.componentRef.instance.errors = this.control.errors;
   }
 
